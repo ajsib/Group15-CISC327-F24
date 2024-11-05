0351d3ca36894f3676fe4b40c52773608b6242da
Will Wu <will24m@users.noreply.github.com>
2024-11-04 20:19:58 -0500

    server side update to accomodate confirm details page

--- server/routes/flight.js
+++ server/routes/flight.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { searchFlights } from '../controllers/flightController.js';
+import { confirmFlightDetails } from '../controllers/confirmDetailsController.js';
 
 const router = express.Router();
 
 router.get('/search-results', searchFlights);
+router.post('/confirm-details', confirmFlightDetails); // New route for confirmation
 
 export default router;
